7904c5847bfdc613c74f58ddf6d0720e9712d768
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-10-29 16:58:02 -0500

    terminado

--- 02-escenario-interactivo/index.js
+++ 02-escenario-interactivo/index.js
@@ -1,3 +1,5 @@
+// ESCENARIO 1 =========================================================================================================0
+
 // Capturar variables
 
 const jell = document.querySelectorAll(".jellyfish") //clases
@@ -15,22 +17,120 @@ jell.forEach( item => {
         item.classList.add("saltar")
         counter++
         tablero.textContent = counter
-    
-    
     })
-
-
-
-
-
-
-
-
 })
 
 
 
+// ESCENARIO 2 ======================================================================================================================
+
+const personajes = document.querySelectorAll(".personajes") //clases
+const contadorPersonajes = document.querySelector("#contador-personajes") //id
+let counterDos = 0
+
+personajes.forEach(item => {
+
+    item.addEventListener("click", () => {
+        if (item.classList.contains("cambioColor")) return;
+        item.classList.add("cambioColor");
+        setTimeout(() => {
+        item.classList.add("saltar");
+
+            
+            setTimeout(() => {
+                item.style.display = "none";
+            }, 600);
+
+        }, 600); 
+
+        counterDos++;
+        contadorPersonajes.textContent = counterDos;
+
+        contadorPersonajes.classList.add("activo");
+        setTimeout(() => contadorPersonajes.classList.remove("activo"), 300);
+    });
+});
+
+
+// ESCENARIO 3 ======================================================================================================================
+
+const caracol = document.getElementById("caracol");
+const contador = document.getElementById("contador-movimientos"); 
+let clics = 0; 
+
+caracol.addEventListener("click", function() {
+  clics = clics + 1;
+
+  contador.textContent = clics;
+
+  if (clics === 1) {
+    caracol.style.transform = "translateX(160px)";
+  }
+
+  else if (clics === 2) {
+    caracol.style.transform = "translateX(160px) translateY(-220px)";
+  }
+
+  else if (clics === 3) {
+    caracol.style.transform = "translateX(350px) translateY(-220px)";
+  }
+
+  else if (clics === 4) {
+    caracol.style.animation = "saltoCaracol 0.6s ease-in-out infinite";
+    caracol.style.transform = "translateX(350px) translateY(-220px)";
+  }
+});
 
 
 
-// 3. Eventos
+// Carrusel  =======================================================================================================================0
+
+//  1.Variables
+ const escenas = document.querySelectorAll(".escena")
+ const anterior = document.querySelector(".anterior")
+ const siguiente = document.querySelector(".siguiente")
+ const miniatura = document.querySelectorAll(".miniatura")
+ let indice = 0
+
+console.log(escenas)
+console.log(anterior)
+console.log(siguiente)
+console.log(miniatura)
+
+// 2.Funciones
+function mostrarEscena(i){
+
+
+    for (let j = 0; j < escenas.length; j++) {
+        escenas[j].classList.remove("activa")
+     }
+
+    escenas[i].classList.add("activa")
+
+    indice = i
+ }
+
+
+mostrarEscena(0)
+
+anterior.addEventListener("click", function(){
+    indice = indice - 1
+    if(indice < 0){
+        indice = escenas.length - 1 
+    }
+ mostrarEscena(indice)
+})
+
+siguiente.addEventListener("click", function(){
+    indice = indice + 1
+    if(indice >= escenas.length){
+        indice = 0
+    }
+    mostrarEscena(indice)
+})
+
+miniatura.forEach((mini, i) => {
+  mini.addEventListener("click", () => {
+    mostrarEscena(i); 
+  });
+});
