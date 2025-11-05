c4c6ad08b35ed5e96686e0d45d72c685a6ae9f00
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-11-05 17:24:10 -0500

    Avance

---
+++ 03-base-datos-comic/bd.js
@@ -0,0 +1,78 @@
+let comic =
+{
+  "nombreComic": "Cuerdas del Destino",
+  "NumeroCapitulos": 3,
+  "genero": "Drama",
+  "sinopsis": "Super tower",
+  "autores": ["Juan Camilo Betancourt", "Brayhan Castillo", "Gabriel Torres"],
+  "portadaComic" : "/img/portada.jpg",
+  "year" : "2025",
+
+  "capitulos": [
+    {
+      "id" : 1,
+      "nombre": "El Descubrimiento",
+      "Personajes": ["Juanes", "Abuelo", "Abuela", "Padre", "HermanoUno", "HermanoDos", "HermanoTres", "HermanoCuatro", "HermanoCinco" ],
+      "portada": "./img/portada.jpg",
+      "descripcion": "En una choza perdida junto al río, un niño descubre una misteriosa caja que cambiará su vida. Dentro, una guitarra dormida espera despertar un destino marcado por la música, el misterio y una promesa que lo acompañará para siempre.",
+    },
+
+    {
+      "id" : 2,
+      "nombre": "Las Líricas",
+      "Personajes": 29,
+      "portada": "./img/portada.jpg",
+      "descripcion": "...",
+    },
+
+    {
+      "id" : 1,
+      "nombre": "De solución",
+      "Personajes": 29,
+      "portada": "./img/portada.jpg",
+      "descripcion": "...",
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
