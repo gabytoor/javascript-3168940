8da837842024dc6c1a79bc7e010afcfac141e356
Gabriel Torres <gabrieltorresortiz159@gmail.com>
2025-12-03 15:30:14 -0500

    Add files via upload

--- 03-base-datos-comic/personajes.js
+++ 03-base-datos-comic/personajes.js
@@ -6,11 +6,33 @@ const params = new URLSearchParams(window.location.search)
 const id = parseInt(params.get("id"))
 
 // Comparar id de URL con id de la base de datos
-const miPersonaje = comic.personajes.find (p => p.id === id)
+const miPersonaje = comic.personajes.find (char => char.id === id);
+
+console.log ("el id del personaje es" + id);
 
 const general = document.querySelector(".general")
 
 console.log(".general")
 
-// hacer el general inner .html
+// hacer el general inner .html                         
 
+
+general.innerHTML = `
+<section class="personaje-detalle">
+
+    <h1 class="titulo-personaje">${miPersonaje.nombre}</h1>
+
+    <div class="img-personaje">
+        <img src="${miPersonaje.img}" alt="${miPersonaje.nombre}">
+    </div>
+
+    <p class="descripcion-personaje">
+        ${miPersonaje.descripcion}
+    </p>
+
+    <div class="video-container">
+        <video src="${miPersonaje.video}" autoplay muted playsinline controls></video>
+    </div>
+
+</section>
+`;
